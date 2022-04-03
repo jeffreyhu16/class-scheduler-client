3efe2731de42baa4cab832157c7da72001377525
Jeffrey <jeffreyhu16@gmail.com>
2022-04-03 12:17:22 +0800

    add react-calendar current date style

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import Calendar from 'react-calendar'
 import { DateTime } from 'luxon'
-// import 'react-calendar/dist/Calendar.css';
+import { dataContext } from './contexts/DataContext';
 
 export default function Sidebar() {
 
+    const { currentDate } = React.useContext(dataContext);
     const [ date, setDate ] = React.useState(new Date());
     const testDate = DateTime.fromISO(date.toISOString());
 
+    const matchTile = date => {
+        if (currentDate) {const tileDate = DateTime.fromISO(date.toISOString());
+        if (tileDate.month === currentDate.month && tileDate.day === currentDate.day)
+            return tyleStyle;}
+    }
+
+    const tyleStyle = 'active-tile';
+
     return (
         <div className="sidebar">
-            <Calendar value={date} onChange={(date) => setDate(date)}/>
+            <Calendar 
+                value={date} 
+                onChange={setDate}
+                tileClassName={({ date }) => matchTile(date)}
+            />
             <ul className="side-list">
             </ul>
         </div>
