65f800b39bf3f59066243543bba8a50e418b722e
Jeffrey <jeffreyhu16@gmail.com>
2022-04-04 19:51:32 +0800

    add env to root

--- backend/App.js
+++ backend/App.js
@@ -9,9 +9,9 @@ db.connectDB();
 IndexRouter.combinedRoute(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static(path.join(__dirname,'../frontend/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'../frontend/build/index.html'))
+        res.sendFile(path.join(__dirname,'/../frontend/build/index.html'))
     });
 }
 
