7083e4a159ae860c41acf859e80c8ed5a5b63c7f
Jeffrey <jeffreyhu16@gmail.com>
2022-04-05 08:59:58 +0800

    add webpack

---
+++ backend/config/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const extract = require("mini-css-extract-plugin");
+let CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '/../../frontend/build'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: extract.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            },
+            {
+                //applying rule
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        //using file-loader
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: "images"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CaseSensitivePathsPlugin(),
+        new extract({
+            filename: 'bundle.css'
+        })
+    ],  
+}
