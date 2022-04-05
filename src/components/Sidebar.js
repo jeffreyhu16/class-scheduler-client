da33dd7f0f189f7e03b71899311b815e14d0f9a3
Jeffrey <jeffreyhu16@gmail.com>
2022-04-05 15:38:16 +0800

    fix safari view

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,21 +7,22 @@ Settings.defaultZone = 'Asia/Taipei';
 export default function Sidebar() {
 
     const { currentDate } = React.useContext(dataContext);
-    const [ date, setDate ] = React.useState(new Date());
-    const testDate = DateTime.fromISO(date.toISOString());
+    const [date, setDate] = React.useState(new Date());
 
     const matchTile = date => {
-        if (currentDate) {const tileDate = DateTime.fromISO(date.toISOString());
-        if (tileDate.month === currentDate.month && tileDate.day === currentDate.day)
-            return tyleStyle;}
+        if (currentDate) {
+            const tileDate = DateTime.fromISO(date.toISOString());
+            if (tileDate.equals(DateTime.fromObject(currentDate)))
+                return tyleStyle;
+        }
     }
 
     const tyleStyle = 'active-tile';
 
     return (
         <div className="sidebar">
-            <Calendar 
-                value={date} 
+            <Calendar
+                value={date}
                 onChange={setDate}
                 tileClassName={({ date }) => matchTile(date)}
             />
